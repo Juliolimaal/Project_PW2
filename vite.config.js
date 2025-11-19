c79545936ad6d71aaea6140c9b4f88a3b57ab58b
sebastiaobernardo <sebastiaobernardo@academico.ifpb.edu.br>
2025-11-19 11:26:44 -0300

    Correções vite

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  plugins: [tailwindcss()],
   
   build: {
     rollupOptions: {
       input: {
-        
         main: resolve(__dirname, 'index.html'), 
-        
-        
         wine_red: resolve(__dirname, 'src/pages/wine_red.html'), 
         wine_rose: resolve(__dirname, 'src/pages/wine_rose.html'),
         wine_white: resolve(__dirname, 'src/pages/wine_white.html'),
